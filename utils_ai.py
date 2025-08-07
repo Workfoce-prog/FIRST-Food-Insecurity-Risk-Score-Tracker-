a1556fd505c6ba85cb1e2f22f1836e1105a3ee1f
Workfoce-prog <ziyackcoulibaly@gmail.com>
2025-08-06 21:43:24 -0500

    Delete utils_ai.py

--- utils_ai.py
+++
@@ -1,8 +0,0 @@
-
-def generate_ai_flags(row):
-    if row["Risk_Score"] > 75:
-        return "Immediate Attention"
-    elif row["Risk_Score"] > 50:
-        return "Follow-Up Needed"
-    else:
-        return "Stable"
